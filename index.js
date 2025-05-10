675c2b752007ddb84cd5566b7ec90f2862bd161f
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-05-10 08:33:54 -0400

    Mongoose Part 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Campsite = require("./models/campsite");
 
 const url = "mongodb://127.0.0.1:27017/nucampsite";
+
 const connect = mongoose.connect(url, {});
 
 connect.then(() => {
@@ -13,10 +14,30 @@ connect.then(() => {
   })
     .then((campsite) => {
       console.log(campsite);
-      return Campsite.find();
+
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: "Updated Test Document" },
+        },
+        {
+          new: true,
+        }
+      );
     })
-    .then((campsites) => {
-      console.log(campsites);
+    .then((campsite) => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+      });
+
+      return campsite.save();
+    })
+    .then((campsite) => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
